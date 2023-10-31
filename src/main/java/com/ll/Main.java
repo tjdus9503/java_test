e3376733f561ff28f55a6118ad2bacc8d5f492ee
tjdus9503 <tjdus03095@gmail.com>
2023-10-31 09:16:22 +0900

    add one line

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,6 +3,7 @@ package com.ll;
 public class Main {
     public static void main(String[] args){
         System.out.println("main 함수가 실행되었습니다.");
+        System.out.println("한 줄 추가했어요.");
     }
 
 
